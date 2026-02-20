854da96b4d3d493105b5efc14cf13fd36ef70ae9
SukbeomH <brent93.dev@gmail.com>
2026-02-20 15:29:35 +0900

    fix(utils): detect_device __init__.py re-export 누락 수정

--- rag_bench/utils/__init__.py
+++ rag_bench/utils/__init__.py
@@ -1,9 +1,11 @@
 """rag_bench 공유 유틸리티 패키지."""
 
+from rag_bench.utils.device import detect_device
 from rag_bench.utils.qa_loader import load_qa_dataset
 from rag_bench.utils.report import print_ragas_table
 
 __all__ = [
+    "detect_device",
     "load_qa_dataset",
     "print_ragas_table",
 ]
